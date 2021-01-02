9c4609c518dcc78aa50bf2a027079224e6ed164b
Igor Kirpinev <kirpinev@yahoo.com>
2021-01-02 20:49:44 +0300

    Обновил зависимости

--- uikit/Button2/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as renderer from 'react-test-renderer'
-import { Button } from './index'
-
-describe('Index page', () => {
-  it('should match the snapshot', () => {
-    const tree = renderer.create(<Button primary>Hello</Button>).toJSON()
-    expect(tree).toMatchSnapshot()
-  })
-})
