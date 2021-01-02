c543653eb7b95d81e9f321c82da5f07fd2699dd0
Igor Kirpinev <kirpinev@yahoo.com>
2021-01-02 20:49:44 +0300

    Обновил зависимости

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,4 @@
 module.exports = {
   '**/*.ts?(x)': () => 'npm run type-check',
   '**/*.(ts|js)?(x)': (filenames) => `npm run lint:ts ${filenames.join(' ')}`,
-  './src/**/*.js': (filenames) => `npm run lint:css ${filenames.join(' ')}`,
-  './src/**/*.ts?(x)': (filenames) => `npm run test ${filenames.join(' ')}`,
 }
